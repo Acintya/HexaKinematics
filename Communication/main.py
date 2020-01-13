2d46bb2a5e65aac6a0c643a0acd7f0fcd16f17a3
ling <lingsheng.hs@gmail.com>
2020-01-13 02:22:27 +0100

    added method to init hexa params

--- Communication/main.py
+++ Communication/main.py
@@ -1,14 +1,15 @@
 from Communication import environment
 from Model import HexaModel
-import Numpy as np
+import numpy as np
 
 def InitHexaModel(model):
     model.name = "Hexa_IWF"
-    model.actuatorPos = ,
-    sphereJointPos, orientationAlpha, orientationBeta, orientationGamma, baseLink, plattformLink
+    #model.actuatorPos = ,
+    #sphereJointPos, orientationAlpha, orientationBeta, orientationGamma, baseLink, plattformLink
 
-hexa_iwf = HexaModel()
+hexa_iwf = HexaModel.HexaModel ("IWF_Hexa", 360, 51.96, 51.6, [0, -120, -120, -240, -240, 0])
 InitHexaModel(hexa_iwf)
+hexa_iwf.InverseKinematic()
 env = environment.UnityEnvironment()
 print ("****** env quit *****")
 
--- Model/HexaModel.py
+++ Model/HexaModel.py
@@ -1,14 +1,56 @@
+import numpy as np
+
 class HexaModel(object):
-    def __init__(self, name, actuatorPos, sphereJointPos, orientationAlpha, orientationBeta, orientationGamma, baseLink,
-                 plattformLink, radiusBase, radiusPlattform, distance):
+    def __init__(self, name, R_B, R_P, d, phi):
         self.name = name
-        self.actuatorPos = actuatorPos,
-        self.sphereJointPos = sphereJointPos,
-        self.orientationAlpha = orientationAlpha,
-        self.orientationBeta = orientationBeta,
-        self.orientationGamma = orientationGamma,
-        self.baseLink = baseLink,
-        self.plattformLink = plattformLink,
-        self.radiusBase = radiusBase,
-        self.radiusPlattform = radiusPlattform,
-        self.distance = distance
+        self.InitHexaParams(R_B, R_P, d, phi)
+
+    def InitHexaParams(self, R_B, R_P, d, phi):
+        if len(phi) != 6:
+            return 'asd'
+
+        self.actuatorPos = []
+        self.sphereJointPos = []
+        self.orientationA = []
+
+        for angle in phi:
+            a_pos = []
+            index = phi.index(angle)
+            a_x = R_B * np.cos((angle + np.power(-1, index) * np.arcsin(d / (2 * R_B))) * np.pi / 180)
+            a_pos.append(a_x)
+            a_y = R_B * np.sin((angle + np.power(-1, index) * np.arcsin(d / (2 * R_B))) * np.pi / 180)
+            a_pos.append(a_y)
+            a_z = 0
+            a_pos.append(a_z)
+            self.actuatorPos.append(a_pos)
+
+            c_pos = []
+            c_x = R_P * np.cos((angle + np.power(-1, index) * np.arcsin(d / (2 * R_P))) * np.pi / 180)
+            c_pos.append(c_x)
+            c_y = R_P * np.sin((angle + np.power(-1, index) * np.arcsin(d / (2 * R_P))) * np.pi / 180)
+            c_pos.append(c_y)
+            c_z = 0
+            c_pos.append(c_z)
+            self.sphereJointPos.append(c_pos)
+
+            orientation = []
+            alpha = -90
+            orientation.append(alpha)
+            beta = angle
+            orientation.append(beta)
+            gamma = 0
+            orientation.append(gamma)
+            self.orientationA.append(orientation)
+
+        self.baseLink = 240
+        self.plattformLink = 564
+
+    def InverseKinematic(self, pos, rotation):
+        self.q_a = []
+        for i in range(6):
+            #k = -2 * self.sphereJointPos[0] * self.baseLink
+            #l = -2 * self.sphereJointPos[1] * self.baseLink
+            #m = self.sphereJointPos[0] * self.sphereJointPos[0] + self.sphereJointPos[1] * self.sphereJointPos[1] + self.sphereJointPos[2] * self.sphereJointPos[2] + self.baseLink * self.baseLink + self.plattformLink * self.plattformLink
+            q_a_i = (np.arctan2(m, np.sqrt(k * k + l * l - m * m) - np.arctan2(k, l))) * 180 / np.pi
+            self.q_a.append(q_a_i)
+        print(self.q_a)
